482decdeae136258b076548741961ad827d0402f
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-12-02 13:34:20 +0100

    sondrelg: Finish day 2, pt. 2

--- sondrelg/day2/src/main.rs
+++ sondrelg/day2/src/main.rs
@@ -2,12 +2,13 @@ use std::{env, fs};
 use std::ops::Index;
 use std::path::Path;
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Clone)]
 enum Strategy {
     Rock,
     Paper,
     Scissor,
 }
+
 
 impl Strategy {
     fn from_char(c: &str) -> Self {
@@ -15,18 +16,23 @@ impl Strategy {
             "A" => Self::Rock,
             "B" => Self::Paper,
             "C" => Self::Scissor,
-            "X" => Self::Rock,
-            "Y" => Self::Paper,
-            "Z" => Self::Scissor,
             _ => panic!("Unhandled strategy: {c}")
         }
     }
 
-    fn from_opponent_strategy(strategy: &Strategy) -> Self {
-        match strategy {
-            Self::Rock => Self::Paper,
-            Self::Paper => Self::Scissor,
-            Self::Scissor => Self::Rock,
+    fn from_desired_outcome(outcome: &Outcome, opponent_strategy: &Strategy) -> Self {
+        match outcome {
+            Outcome::Draw => opponent_strategy.clone(),
+            Outcome::Loss => match opponent_strategy {
+                Self::Rock => Self::Scissor,
+                Self::Paper => Self::Rock,
+                Self::Scissor => Self::Paper,
+            },
+            Outcome::Win => match opponent_strategy {
+                Self::Rock => Self::Paper,
+                Self::Paper => Self::Scissor,
+                Self::Scissor => Self::Rock,
+            },
         }
     }
 
@@ -39,6 +45,7 @@ impl Strategy {
     }
 }
 
+#[derive(Debug)]
 enum Outcome {
     Loss,
     Draw,
@@ -46,10 +53,19 @@ enum Outcome {
 }
 
 impl Outcome {
+    fn from_char(c: &str) -> Self {
+        match c {
+            "X" => Self::Loss,
+            "Y" => Self::Draw,
+            "Z" => Self::Win,
+            _ => panic!("Unhandled outcome: {c}")
+        }
+    }
+
     fn from_strategies(opponent_strategy: &Strategy, my_strategy: &Strategy) -> Self {
-        if my_strategy == &Strategy::from_opponent_strategy(opponent_strategy) {
+        if my_strategy == &Strategy::from_desired_outcome(&Outcome::Win, opponent_strategy) {
             Self::Win
-        } else if opponent_strategy == &Strategy::from_opponent_strategy(my_strategy) {
+        } else if opponent_strategy == &Strategy::from_desired_outcome(&Outcome::Loss, my_strategy) {
             Self::Loss
         } else {
             Self::Draw
@@ -66,21 +82,28 @@ impl Outcome {
 }
 
 
-fn main() {
-    let input_path = format!("{}/src/input", &env::var("CARGO_MANIFEST_DIR").unwrap());
-    println!("{}", input_path);
-    let path = Path::new(&input_path);
-    let contents = fs::read_to_string(path).unwrap();
-    let points: u16 = contents.lines().into_iter().map(|line| {
-        // Load choices
+fn calc(content: String) -> u16 {
+    // Iterate over each set of items
+    content.lines().into_iter().map(|line| {
+        // Parse choices
         let split = line.split(" ").collect::<Vec<&str>>();
-        let (opponent_choice, my_choice) = (split.index(0), split.index(1));
+        let (opponent_choice, outcome) = (split.index(0), split.index(1));
+
         // Convert choice to strategy
         let opponent_strategy = Strategy::from_char(opponent_choice);
-        let my_strategy = Strategy::from_char(my_choice);
-        // Calculate score
-        Outcome::from_strategies(&opponent_strategy, &my_strategy).points() + my_strategy.points()
-    }).sum();
+        let desired_outcome = Outcome::from_char(outcome);
+        let strategy = Strategy::from_desired_outcome(&desired_outcome, &opponent_strategy);
 
+        // Calculate score
+         strategy.points() + desired_outcome.points()
+    }).sum()
+}
+
+fn main() {
+    // Load input
+    let input_path = format!("{}/src/input", &env::var("CARGO_MANIFEST_DIR").unwrap());
+    let path = Path::new(&input_path);
+    let content = fs::read_to_string(path).unwrap();
+    let points = calc(content);
     println!("The game produced {} points", points)
 }
