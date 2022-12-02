417a808abcca45855dd1ed9ca6b4c13c1b8dc293
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-12-02 12:53:58 +0100

    sondrelg: Revise solution after understanding how AoC works :)

--- sondrelg/day2/src/main.rs
+++ sondrelg/day2/src/main.rs
@@ -1,3 +1,7 @@
+use std::{env, fs};
+use std::ops::Index;
+use std::path::Path;
+
 #[derive(Eq, PartialEq)]
 enum Strategy {
     Rock,
@@ -6,12 +10,15 @@ enum Strategy {
 }
 
 impl Strategy {
-    fn from_char(c: &char) -> Self {
+    fn from_char(c: &str) -> Self {
         match c {
-            'A' => Self::Rock,
-            'B' => Self::Paper,
-            'C' => Self::Scissor,
-            _ => panic!("Unhandled strategy")
+            "A" => Self::Rock,
+            "B" => Self::Paper,
+            "C" => Self::Scissor,
+            "X" => Self::Rock,
+            "Y" => Self::Paper,
+            "Z" => Self::Scissor,
+            _ => panic!("Unhandled strategy: {c}")
         }
     }
 
@@ -23,7 +30,7 @@ impl Strategy {
         }
     }
 
-    fn points(self) -> u8 {
+    fn points(self) -> u16 {
         match self {
             Self::Rock => 1,
             Self::Paper => 2,
@@ -49,7 +56,7 @@ impl Outcome {
         }
     }
 
-    fn points(&self) -> u8 {
+    fn points(&self) -> u16 {
         match self {
             Self::Loss => 0,
             Self::Draw => 3,
@@ -60,16 +67,20 @@ impl Outcome {
 
 
 fn main() {
-    let opponent_choices = ['A', 'B', 'C'];
-
-    let points: u8 = opponent_choices.iter().map(|opponent_choice| {
+    let input_path = format!("{}/src/input", &env::var("CARGO_MANIFEST_DIR").unwrap());
+    println!("{}", input_path);
+    let path = Path::new(&input_path);
+    let contents = fs::read_to_string(path).unwrap();
+    let points: u16 = contents.lines().into_iter().map(|line| {
+        // Load choices
+        let split = line.split(" ").collect::<Vec<&str>>();
+        let (opponent_choice, my_choice) = (split.index(0), split.index(1));
         // Convert choice to strategy
         let opponent_strategy = Strategy::from_char(opponent_choice);
-        // Choose a winning strategy
-        let my_strategy = Strategy::from_opponent_strategy(&opponent_strategy);
+        let my_strategy = Strategy::from_char(my_choice);
         // Calculate score
         Outcome::from_strategies(&opponent_strategy, &my_strategy).points() + my_strategy.points()
     }).sum();
 
-    println!("The optimal strategy results in {} points", points)
+    println!("The game produced {} points", points)
 }
