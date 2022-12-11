add9bd79d825fd45cc32dd562139df88b0e7e6e2
larsjr <larsroine@gmail.com>
2022-12-11 20:50:48 +0100

    Prepare for part 2, day 10

--- larsjr/day_11/day_10_solution.py
+++ larsjr/day_11/day_10_solution.py
@@ -8,22 +8,23 @@ class Monkey:
     number: int
     items: list[int]
     operation: Callable
-    test: Callable
+    divisor: int
     true_condition: int
-    false_condidtion: int
+    false_condition: int
     number_inspected_items: int = 0
 
-    def take_turn(self, monkeys: list[Monkey]):
+    def take_turn(self, monkeys: list[Monkey], divide_worry_level=False):
         while self.items:
             item = self.items.pop(0)
             self.number_inspected_items += 1
             worry_level = self.operation(item)
-            worry_level = int(worry_level / 3)
+            if divide_worry_level:
+                worry_level = int(worry_level / 3)
 
-            if self.test(worry_level):
+            if worry_level % self.divisor == 0:
                 monkeys[self.true_condition].items.append(worry_level)
             else:
-                monkeys[self.false_condidtion].items.append(worry_level)
+                monkeys[self.false_condition].items.append(worry_level)
 
 
 def parse_monkey_number(line: str) -> int:
@@ -41,9 +42,8 @@ def parse_operation(line: str) -> Callable:
     return eval(f"lambda old: {operation[1].strip()}")
 
 
-def parse_test(line: str) -> Callable:
-    divide_by = int(line.split()[-1].strip())
-    return lambda x: x % divide_by == 0
+def parse_divisor(line: str) -> int:
+    return int(line.split()[-1].strip())
 
 
 def parse_condition(line: str) -> int:
@@ -54,7 +54,7 @@ def create_monkey(lines: list[str]) -> Monkey:
     monkey_number = parse_monkey_number(lines[0])
     items = parse_starting_items(lines[1])
     operation = parse_operation(lines[2])
-    test = parse_test(lines[3])
+    divisor = parse_divisor(lines[3])
     true_condition = parse_condition(lines[4])
     false_condition = parse_condition(lines[5])
 
@@ -62,9 +62,9 @@ def create_monkey(lines: list[str]) -> Monkey:
         number=monkey_number,
         items=items,
         operation=operation,
-        test=test,
+        divisor=divisor,
         true_condition=true_condition,
-        false_condidtion=false_condition,
+        false_condition=false_condition,
     )
 
 
@@ -78,16 +78,16 @@ def parse_lines(lines: list[str]) -> list[Monkey]:
     return monkeys
 
 
-def play_round(monkeys: list[Monkey]) -> None:
+def play_round(monkeys: list[Monkey], divide_worry_level: bool) -> None:
     for monkey in monkeys:
-        monkey.take_turn(monkeys)
+        monkey.take_turn(monkeys, divide_worry_level)
 
 
 def find_two_most_active_afer_rounds(
-    num_rounds: int, monkeys: list[Monkey]
+    num_rounds: int, monkeys: list[Monkey], divide_worry_level: bool
 ) -> tuple[Monkey, Monkey]:
     for _ in range(num_rounds):
-        play_round(monkeys)
+        play_round(monkeys, divide_worry_level)
 
     sorted_monkeys = sorted(monkeys, key=lambda m: m.number_inspected_items)
     return (sorted_monkeys[-1], sorted_monkeys[-2])
@@ -99,5 +99,16 @@ if __name__ == "__main__":
 
     monkeys = parse_lines(lines)
 
-    m1, m2 = find_two_most_active_afer_rounds(20, monkeys)
-    print(f"Solution part 1: {m1.number_inspected_items * m2.number_inspected_items}")
+    m1_part1, m2_part1 = find_two_most_active_afer_rounds(
+        20, monkeys[:], divide_worry_level=True
+    )
+    print(
+        f"Solution part 1: {m1_part1.number_inspected_items * m2_part1.number_inspected_items}"
+    )
+
+    m1_part2, m2_part2 = find_two_most_active_afer_rounds(
+        10000, monkeys[:], divide_worry_level=False
+    )
+    print(
+        f"Solution part 2: {m1_part2.number_inspected_items * m2_part2.number_inspected_items}"
+    )
